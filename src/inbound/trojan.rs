b7c5e1d589a485006bd0d950a1a24f6794cbdf6e
spacemeowx2 <spacemeowx2@gmail.com>
2021-05-14 01:21:56 +0800

    feat: add UdpCodec

--- src/inbound/trojan.rs
+++ src/inbound/trojan.rs
@@ -1,7 +1,12 @@
-use anyhow::{bail, Result};
-use tokio::io::{AsyncRead, AsyncWrite};
+use std::io;
 
-use crate::utils::peekable_stream::PeekableStream;
+use anyhow::{bail, Result};
+use bytes::{Buf, BufMut, BytesMut};
+use socks5_protocol::{sync::FromIO, Address};
+use tokio::io::{AsyncRead, AsyncWrite};
+use tokio_util::codec::{Decoder, Encoder};
+
+use crate::{outbound::UdpPacket, utils::peekable_stream::PeekableStream};
 
 #[derive(Clone)]
 pub struct TrojanAcceptor {}
@@ -19,5 +24,73 @@ impl TrojanAcceptor {
         stream.peek_exact(&mut buf).await?;
         bail!("todo")
         // Don't forget to stream.drain(10)
+    }
+}
+
+const UDP_MAX_SIZE: usize = 65535;
+// 259 is max size of address, atype 1 + domain len 1 + domain 255 + port 2
+const PREFIX_LENGTH: usize = 259 + 2 + 2;
+
+struct UdpCodec;
+
+impl Encoder<UdpPacket> for UdpCodec {
+    type Error = io::Error;
+
+    fn encode(&mut self, item: UdpPacket, dst: &mut BytesMut) -> Result<(), Self::Error> {
+        if item.0.len() > UDP_MAX_SIZE {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame of length {} is too large.", item.0.len()),
+            ));
+        }
+
+        dst.reserve(PREFIX_LENGTH + item.0.len());
+        let mut writer = dst.writer();
+
+        Address::from(item.1)
+            .write_to(&mut writer)
+            .map_err(|e| e.to_io_err())?;
+        let dst = writer.into_inner();
+
+        dst.put_u16(item.0.len() as u16);
+        dst.extend_from_slice(&[0x0D, 0x0A]);
+        dst.extend_from_slice(&item.0);
+
+        Ok(())
+    }
+}
+
+impl Decoder for UdpCodec {
+    type Item = UdpPacket;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        let mut reader = src.reader();
+        let result = Address::read_from(&mut reader);
+        let src = reader.into_inner();
+
+        let address = match result {
+            Ok(a) => a,
+            Err(socks5_protocol::Error::Io(e)) if e.kind() == io::ErrorKind::UnexpectedEof => {
+                return Ok(None);
+            }
+            Err(e) => return Err(e.to_io_err()),
+        };
+        if src.remaining() < 4 {
+            return Ok(None);
+        }
+
+        let length = src.get_u16();
+        // CrLf
+        src.get_u16();
+
+        let mut buf = vec![0u8; length as usize];
+
+        src.copy_to_slice(&mut buf);
+
+        Ok(Some((
+            buf,
+            address.to_socket_addr().map_err(|e| e.to_io_err())?,
+        )))
     }
 }
