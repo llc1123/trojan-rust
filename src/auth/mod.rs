314a9fb9f2a2ac8314c2c0c73e7b77f46197efd7
粒粒橙 <i@llc.moe>
2021-05-14 22:18:08 +0800

    refine control flow

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -20,16 +20,16 @@ pub struct AuthHub {
 impl AuthHub {
     pub async fn new(config: &Config) -> Result<AuthHub> {
         let config_auth = ConfigAuthenticator::new(config.trojan.password)?;
-        match config.redis {
-            Some(redis) => Ok(AuthHub {
+        if let Some(redis) = config.redis {
+            return Ok(AuthHub {
                 config_auth,
                 redis_auth: Some(RedisAuthenticator::new(redis.server)?),
-            }),
-            None => Ok(AuthHub {
-                config_auth,
-                redis_auth: None,
-            }),
+            });
         }
+        Ok(AuthHub {
+            config_auth,
+            redis_auth: None,
+        })
     }
 }
 
@@ -37,29 +37,23 @@ impl AuthHub {
 impl Auth for AuthHub {
     async fn auth(&self, password: String) -> Result<bool> {
         if self.config_auth.auth(password).await? {
-            Ok(true)
-        } else {
-            match self.redis_auth {
-                Some(redis) => Ok(redis.auth(password).await?),
-                None => Ok(false),
-            }
+            return Ok(true);
         }
+        if let Some(redis) = self.redis_auth {
+            return Ok(redis.auth(password).await?);
+        }
+        Ok(false)
     }
 
     async fn stat(&self, password: String, upload: u64, download: u64) -> Result<()> {
         if self.config_auth.auth(password).await? {
-            Ok(self.config_auth.stat(password, upload, download).await?)
-        } else {
-            match self.redis_auth {
-                Some(redis) => {
-                    if redis.auth(password).await? {
-                        Ok(redis.stat(password, upload, download).await?)
-                    } else {
-                        Err(anyhow!("User {} not found.", &password))
-                    }
-                }
-                None => Err(anyhow!("User {} not found.", &password)),
+            return Ok(self.config_auth.stat(password, upload, download).await?);
+        }
+        if let Some(redis) = self.redis_auth {
+            if redis.auth(password).await? {
+                return Ok(redis.stat(password, upload, download).await?);
             }
         }
+        Err(anyhow!("User {} not found.", &password))
     }
 }
