4096fe4d77cb2769f37a57aa9c6022c041b12dbf
粒粒橙 <i@llc.moe>
2021-05-17 03:11:09 +0800

    fix

--- src/inbound/trojan.rs
+++ src/inbound/trojan.rs
@@ -54,7 +54,7 @@ impl TrojanAcceptor {
         stream.peek_exact(&mut buf).await?;
 
         let password = String::from_utf8_lossy(&buf[0..56]);
-        if let Err(_) = hex::decode(password) {
+        if let Err(_) = hex::decode(password.as_ref()) {
             bail!("Not trojan request.")
         }
         if !self.auth_hub.auth(&password).await? {
