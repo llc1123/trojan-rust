7a07f77f8957c1337ba27bb747e180bcb3756b59
粒粒橙 <i@llc.moe>
2021-05-20 13:18:22 +0800

    add check_private_key

--- src/inbound/tls_openssl.rs
+++ src/inbound/tls_openssl.rs
@@ -5,12 +5,12 @@ use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod, SslRef, SslVerifyMode};
 fn keylog_callback(ssl: &SslRef, s: &str) {}
 
 pub fn new(config: &Tls) -> Result<SslAcceptor> {
-    let mut builder = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls_server())?;
-    builder.set_verify(SslVerifyMode::NONE);
-    builder.set_certificate_chain_file(&config.cert)?;
-    builder.set_private_key_file(&config.key, SslFiletype::PEM)?;
-    builder.set_keylog_callback(keylog_callback);
-    let acceptor = builder.build();
+    let mut acceptor = SslAcceptor::mozilla_intermediate_v5(SslMethod::tls_server())?;
+    acceptor.set_verify(SslVerifyMode::NONE);
+    acceptor.set_certificate_chain_file(&config.cert)?;
+    acceptor.set_private_key_file(&config.key, SslFiletype::PEM)?;
+    acceptor.check_private_key()?;
+    acceptor.set_keylog_callback(keylog_callback);
 
-    Ok(acceptor)
+    Ok(acceptor.build())
 }
